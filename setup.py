464caa02342ba046bb1d66c445aa0eacc16a194e
heyDev07 <295devraj@gmail.com>
2025-07-09 11:47:26 +0530

    final

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
-HYPEN_E_DOT='-e .'
+
+HYPHEN_E_DOT = '-e .'
+
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function will return the list of requirements
+    This function will return list of requirements
+    from requirements.txt file
     '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+    # Initializing blank requirements list
+    requirements = []
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-            
-    return requirements    
+    # Opening the file
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        # Remove hyphen_e_dot if present in requirements
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
 
 setup(
-    name='MLPROJECT',
-    version='0.0.1',
-    author='Dev',
-    author_email='295devraj@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
-
+    name = 'mlproject_regression',
+    version= '0.0.1',
+    author='Utkarsh Gaikwad',
+    author_email='gaikwadujg@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
 )
